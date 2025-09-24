708484f4e5836f72fd8a6c021621b5ed7c9aa271
Jacob Evans <jevan234@students.kennesaw.edu>
2025-09-23 20:20:37 -0400

    Added initial definitions of screen size and colors.

--- test.py
+++ test.py
@@ -6,3 +6,15 @@ from time import sleep
 
 #initialize pygame
 pg.init()
+
+# Define screen size
+WIDTH, HEIGHT = 1920, 1080
+SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Test Game")
+
+# Define Screen Colors
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
